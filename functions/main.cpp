26cc980bbe8dab044c00500ec9c3d9d5b2135125
MarkWengSTR <bskin0330@gmail.com>
2021-07-15 16:38:26 +0800

    C++ funcitons study

---
+++ functions/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int Multiply(int a, int b)
+{
+  return a * b;
+}
+
+void MultiplyAndLog(int a, int b)
+{
+  int result = Multiply(a, b);
+  std::cout << result << std::endl;
+}
+
+int main()
+{
+  MultiplyAndLog(2, 3);
+  MultiplyAndLog(10, 20);
+  MultiplyAndLog(100, 300);
+  std::cin.get();
+}
